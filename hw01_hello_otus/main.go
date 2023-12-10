a0c20a057d7d45e797ed7e12dc272d49a11ee704
anastasiya.perelygin <anastasiya.perelygina@dif.tech>
2023-12-10 12:01:39 +0200

    HW1 is completed

--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"fmt"
+	"golang.org/x/example/hello/reverse"
+)
+
 func main() {
-	// Place your code here.
+	helloStr := "Hello, OTUS!"
+	reversedHelloStr := reverse.String(helloStr)
+	fmt.Println(reversedHelloStr)
 }
